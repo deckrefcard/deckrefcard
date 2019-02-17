eefb4013d6efea6e19bfda31daad3d14c5618486
Sean Goller <sean@goller.net>
2019-02-17 03:27:04 -0800

    Initial revision.

---
+++ lib/deckrefcard.rb
@@ -0,0 +1,6 @@
+require "deckrefcard/version"
+
+module Deckrefcard
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/deckrefcard/version.rb
@@ -0,0 +1,3 @@
+module Deckrefcard
+  VERSION = "0.1.0"
+end
